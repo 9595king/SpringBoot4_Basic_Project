c749b607d99bcd0cff1dacb274cd42c1db203a7f
9595king <9595king@gmail.com>
2026-08-31 15:35:59 +0900

    2026.08.31 Updates

--- src/main/java/com/rookies6/MySpringBoot4Project/MySpringBoot4ProjectApplication.java
+++ src/main/java/com/rookies6/MySpringBoot4Project/MySpringBoot4ProjectApplication.java
@@ -3,18 +3,17 @@ package com.rookies6.myspringboot4project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.micrometer.metrics.autoconfigure.MetricsProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MySpringBoot4ProjectApplication {
 
 	public static void main(String[] args) {
-        //SpringApplication.run(MySpringBoot4ProjectApplication.class, args);
+        //SpringApplication.run(MySp1ringBoot4ProjectApplication.class, args);
         SpringApplication application = new SpringApplication(MySpringBoot4ProjectApplication.class);
         //WebApplication type 변경
-        //AnnotationConfigService
-        application.setWebApplicationType(WebApplicationType.NONE);
+        //자동으로 AnnotationConfigServletWebServerApplicationContext 컨테이너 객체가 생성됨
+        application.setWebApplicationType(WebApplicationType.SERVLET);
         application.run(args);
     }
 
