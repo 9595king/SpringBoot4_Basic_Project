5f9c5d095d88a46ce736846eb5bc49e5b938a025
9595king <9595king@gmail.com>
2026-09-04 13:39:10 +0900

    2026.09.04 Updates

---
+++ src/main/java/com/rookies6/MySpringBoot4Project/auth/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.rookies6.myspringboot4project.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/users/welcome").permitAll()
+                            .requestMatchers("/api/users/**").authenticated();
+                })
+                .formLogin(withDefaults())
+                .build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        //ADMIN 역할 사용자 생성
+        UserDetails admin = User.withUsername("adminboot")
+                .password(encoder.encode("pwd1"))
+                .roles("ADMIN")
+                .build();
+        //User 역할 사용자 생성
+        UserDetails user = User.withUsername("userboot")
+                .password(encoder.encode("pwd2"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
