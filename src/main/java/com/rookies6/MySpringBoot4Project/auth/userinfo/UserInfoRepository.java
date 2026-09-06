513e85d510de74a946e343714bb1b39d5c096324
9595king <9595king@gmail.com>
2026-09-06 11:56:07 +0900

    2026.09.04 Updates

--- src/main/java/com/rookies6/MySpringBoot4Project/auth/userinfo/UserInfoRepository.java
+++ src/main/java/com/rookies6/MySpringBoot4Project/auth/userinfo/UserInfoRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.ListCrudRepository;
 import java.util.Optional;
 
 public interface UserInfoRepository extends ListCrudRepository<UserInfo, Integer> {
+    //인증할 때 username의 값은 email 주소로 입력함
 Optional<UserInfo> findByEmail(String email);
 }
